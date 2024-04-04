563b90c49c5d3ec4430525ccf5a4096ede4660ae
Nicolas Seine <nicolasseine.dev@gmail.com>
2024-04-04 02:41:26 +0200

    add seo extension

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,17 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({ applyBaseStyles: false }), react()],
+  site: "https://nicolasseine.com",
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    react(),
+    sitemap(),
+  ],
 });
